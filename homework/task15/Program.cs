9e132fdba430afda1fcfb406de142c35b7a427d4
Anna Kerps <anya-kerps@bk.ru>
2022-06-28 00:42:11 +0300

    Задача 15

---
+++ homework/task15/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.WriteLine("Введите число от 1 до 7");
+int weeknumber = Convert.ToInt32(Console.ReadLine());
+if (weeknumber <= 5 & weeknumber >= 1)
+{
+    Console.WriteLine("нет");
+}
+else if (weeknumber == 6)
+{
+    Console.WriteLine("да");
+}
+else if (weeknumber == 7)
+{
+    Console.WriteLine("да");
+}
